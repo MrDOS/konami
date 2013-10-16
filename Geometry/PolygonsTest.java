78145063b0b9c8eb96009445f1680bd0890754bb
Samuel Coleman <samuel@seenet.ca>
2013-10-16 11:56:40 -0300

    Add unit test for Polygons.java.

---
+++ Geometry/PolygonsTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import java.awt.geom.Point2D;
+
+import static org.junit.Assert.assertEquals;
+
+public class PolygonsTest
+{
+    @Test
+    public void testAreaRectangle() throws Exception {
+        assertEquals(1, Polygons.area(new Point2D[] {
+            new Point2D.Double(0, 0),
+            new Point2D.Double(1, 0),
+            new Point2D.Double(0, 1),
+            new Point2D.Double(1, 1)
+        }), 0);
+
+        assertEquals(4, Polygons.area(new Point2D[] {
+            new Point2D.Double(0, 0),
+            new Point2D.Double(2, 0),
+            new Point2D.Double(0, 2),
+            new Point2D.Double(2, 2)
+        }), 0);
+    }
+}
