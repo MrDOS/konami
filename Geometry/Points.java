8ffb35565a486b2957c35ead38bd99bd401d854a
Samuel Coleman <samuel@seenet.ca>
2013-10-17 11:15:10 -0300

    Messy, partially-functional convex hull.

--- Geometry/Points.java
+++ Geometry/Points.java
@@ -1,5 +1,7 @@
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Points
 {
@@ -13,5 +15,62 @@ public class Points
         double side = (line.x2 - line.x1) * (point.y - line.y1)
                     - (line.y2 - line.y1) * (point.x - line.x1);
         return (side < 0) ? Side.RIGHT : (side > 0) ? Side.LEFT : Side.ON;
+    }
+
+    /**
+     * Discover the convex hull of a collection of points. Uses QuickHull.
+     * @param points the points
+     * @return the points that form the convex hull
+     */
+    public static List<Point2D.Double> convexHull(List<Point2D.Double> points) {
+        Point2D.Double left = new Point2D.Double(Double.MAX_VALUE, 0);
+        Point2D.Double right = new Point2D.Double(Double.MIN_VALUE, 0);
+        for (Point2D.Double point : points) {
+            if (point.x < left.x)
+                left = point;
+            if (point.x > right.x)
+                right = point;
+        }
+
+        ArrayList<Point2D.Double> hull = new ArrayList<Point2D.Double>();
+        /* In order to retain compatibility with Polygons.area(), we build the
+         * hull counter-clockwise. */
+        hull.add(right);
+        hull.add(left);
+        int hullSize = 2;
+
+        int startingIndex = 0;
+        boolean pastFirst = false;
+        Line2D.Double segment;
+        Point2D.Double point;
+        do {
+            segment = new Line2D.Double(hull.get(startingIndex),
+                hull.get((startingIndex + 1) % hullSize));
+            point = furthestPoint(segment, points, Side.RIGHT);
+            if (point != null) {
+                hull.add(startingIndex + 1, point);
+                hullSize++;
+            }
+            else {
+                startingIndex = (startingIndex + 1) % hull.size();
+                pastFirst = true;
+            }
+        } while (startingIndex != 0 || !pastFirst);
+
+        return hull;
+    }
+
+    /**
+     * Determine the furthest point in a group from a line.
+     */
+    public static Point2D.Double furthestPoint(Line2D.Double line,
+                                               List<Point2D.Double> points,
+                                               Side side) {
+        Point2D.Double furthest = null;
+        double distance = Double.MIN_VALUE;
+        for (Point2D.Double point : points)
+            if (side(line, point) == side && line.ptLineDist(point) > distance)
+                furthest = point;
+        return furthest;
     }
 }
--- Geometry/PointsTest.java
+++ Geometry/PointsTest.java
@@ -2,6 +2,10 @@ import org.junit.Test;
 
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,5 +22,34 @@ public class PointsTest
         /* on line */
         assertEquals(Points.Side.ON, Points.side(new Line2D.Double(0, 0, 2, 0), new Point2D.Double(1, 0)));
         assertEquals(Points.Side.ON, Points.side(new Line2D.Double(0, 0, 0, 2), new Point2D.Double(0, 1)));
+    }
+
+    @Test
+    public void testConvexHull() throws Exception {
+        List<Point2D.Double> diamond = Arrays.asList(
+            new Point2D.Double(1, 0),
+            new Point2D.Double(0, 1),
+            new Point2D.Double(-1, 0),
+            new Point2D.Double(0, -1)
+        );
+
+        List<Point2D.Double> square = Arrays.asList(
+            new Point2D.Double(1, 1),
+            new Point2D.Double(-1, 1),
+            new Point2D.Double(-1, -1),
+            new Point2D.Double(1, -1)
+        );
+
+        List<Point2D.Double> noisyDiamond = new ArrayList<Point2D.Double>(diamond);
+        noisyDiamond.add(new Point2D.Double(0, 0));
+
+        List<Point2D.Double> noisySquare = new ArrayList<Point2D.Double>(square);
+        Random random = new Random();
+        for (int i = 0; i < 100; i++)
+            noisySquare.add(new Point2D.Double(random.nextDouble() * 2 - 1, random.nextDouble() * 2 - 1));
+
+        assertEquals(diamond, Points.convexHull(diamond));
+        assertEquals(square, Points.convexHull(square));
+        assertEquals(diamond, Points.convexHull(noisyDiamond));
     }
 }
